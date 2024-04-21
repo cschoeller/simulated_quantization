ddf850472a2067a91dd1503d616e76c96457eabe
cschoeller <chschoeller@web.de>
2024-04-21 15:43:53 +0200

    Added some comments

--- multi_layer_quantization.py
+++ multi_layer_quantization.py
@@ -41,24 +41,27 @@ class LinLayer:
     """Simple randomly initialized linear layer without activation."""
 
     def __init__(self, n_in, n_out):
+        """Initialize internal parameters."""
         self.W = np.random.uniform(size=(n_in, n_out), low=-1., high=1.)
         self.b = np.random.uniform(size=(n_out,), low=-0.1, high=0.1)
 
     def __call__(self, x):
+        """Compute the layer's forward pass."""
         return  np.matmul(x, self.W) + self.b
 
 
 class QuantizedLinLayer:
     """Quantization layer that converts a linear layer.
     
-    The computations taking placed are:
+    Explanation:
+    The computations taking place are:
 
     (sx * sp) * yq = (sx * xq) * (sp * Wq) + (sx * sp * bq)
 
     where sx is the scale factor of the quantized input xq and sp is the scale
-    factor of the internal parameters Wq and bq.
+    factor of the internal quantized parameters Wq and bq.
 
-    As we accumulate into int32 to avoid overflows, we requantize yq into yqr
+    As we accumulate into int32 to avoid overflows, we requantize yq into int8 yqr
     before returning:
     
     yq = sr * yqr
@@ -66,22 +69,32 @@ class QuantizedLinLayer:
     Hence, the complete scale factor that dequantizes the layer's output is:
 
     s = sx * sp * sr
-
-    
     """
 
     def __init__(self, linear_layer, x_calib, s_calib):
+        """Copies parameters of the linear layer and calibrate scale factors."""
+        # quantize internal parameters
         s_params = compute_scale([linear_layer.W, linear_layer.b])
         self.Wq = quantize(linear_layer.W, s_params)
         self.bq = quantize(linear_layer.b, s_params * s_calib)
+
+        # compute scale factor for recalibration int float and int32
+        # As we mostly get large values and a large scale factor in requantization,
+        # converting the scale factor to int32 does not incur large inaccuracies. But it
+        # allows us to stay in int for the whole forward pass.
         s_req_precise = self._calibrate(x_calib)
+        print(s_req_precise)
         self.s_req = np.round(s_req_precise).astype(np.int32)
+
+        # compute the final output scale factor, use the precise recalibration scale factor
         self.output_scale = s_params * s_calib * s_req_precise
         
     def _apply_linear(self, xq):
+        """Apply the linear parameters and accumulate into int32."""
         return np.matmul(xq.astype(np.int32), self.Wq.astype(np.int32)) + self.bq.astype(np.int32)
     
     def __call__(self, xq):
+        """Compute the layer's forward pass."""
         yq = self._apply_linear(xq)
         return quantize(yq, self.s_req) # requantize
                 
